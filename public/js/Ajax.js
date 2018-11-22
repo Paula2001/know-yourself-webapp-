38218187f4a4dab9b60cf92a1227088233c5f409
Paula George <polagorge@gmail.com>
2018-11-22 17:00:50 +0200

    Ajax is finished

--- public/js/Ajax.js
+++ public/js/Ajax.js
@@ -6,15 +6,18 @@
  */
 class Ajax {
 
-    constructor(){
+    constructor(searchBool = false ,postBool = false){
         this.arr = [];
         this.lenOfMainArr = null ;
+        this.input = null;
+        this.searchBool = searchBool;
+        this.postBool = postBool ;
     }
 
     /**
      *Ajax request to the  controller
      *
-     * @param controller which will get array from the controller
+     * @param {string} inputId
      *
      * the function sends two ajax request one for index to fireup dispatch function
      *
@@ -22,16 +25,16 @@ class Ajax {
      *
      * @return void
      */
-    request(value = 'name') {
+    request(inputId) {
         let self = this  ;
         $(document).ready(function() {
-            let name = $('#' + value).val();
-            if (name == '' ||name == ' ' ) {
+            self.input = $('#' + inputId).val();
+            if (self.input == '' ||self.input == ' ' ) {
                 self.execute('[]');
             }else{
                 jQuery.ajax({
                     type: 'post',
-                    data: {ajax: 1, name: name},
+                    data: {ajax: 1, name: self.input},
                     success: function () {
                         jQuery.ajax({
                             url: 'data.php',
@@ -48,120 +51,126 @@ class Ajax {
         });
     }
     /**
-     *Creating Elements and pushing results into the view
-     *
-     * @param json object
-     *
-     * @return void
-     *
-     */
-    createElements(arr){
-        let main = document.getElementById('ajax');
-        let table = document.createElement('table');
-        table.id = 'ajaxTable' ;
-        main.appendChild(table);
-        let tableId = document.getElementById('ajaxTable');
-        this.CreateRowsCols(arr);
-        this.set_lenOfMainArr(arr.length);
-    }
-    /**
-     * @param array
-     *
-     * Modify the content
-     *
-     * @return void
-     *
-     */
-    modifyElements(arr){
-        let col = document.getElementsByClassName('link');
-        let count = 0 ;
-        for(let i = 0; i < arr.length;i++){
-            for(let j = 0; j < arr[0].length;j++)
-            {
-                if(j % 2 === 0 ) {
-
-                }else {
-                    col[count].innerHTML = this.replace(arr[i][j]);
-                    count++;
-                }
-            }
-        }
-    }
-
-    /**
-     * @param JSON OBJECT
+     * @param {json} jsonObj
      *
      * On Ajax Success function
      *
      * @return void
      */
     execute(jsonObj){
-        let array =  JSON.parse(jsonObj);
-        alert(array);
-        if(this.lenOfMainArr === array.length){
-            this.modifyElements(array);
+        this.arr =  JSON.parse(jsonObj);
+        if(this.lenOfMainArr === this.arr.length){
+            this.modifyElements();
         }else if(this.lenOfMainArr === null) {
-            this.createElements(array);
-        }else if (this.lenOfMainArr != array.length){
-            this.addRemove(array,array.length);
+            this.createElements();
+        }else if (this.lenOfMainArr !== this.arr.length){
+            this.addRemove();
         }
     }
-
     /**
-     *
-     * @param array , int
-     *
-     * Add or Remove rows
+     *Creating Main Elements
      *
      * @return void
+     *
      */
-    addRemove(arr,arrlen){
-        let row = document.getElementsByClassName('ajaxRow');
-        //remove and modify
-        if(this.lenOfMainArr > arrlen){
-            for(let i = this.lenOfMainArr - 1; i > arrlen - 1 ;i--){
-                row[i].parentNode.removeChild(row[i]);
-            }
-        }
-        //add and modify
-        if(this.lenOfMainArr < arrlen){
-            this.CreateRowsCols(arr,this.lenOfMainArr);
-        }
-        this.lenOfMainArr = arrlen;
-        this.modifyElements(arr);
-
+    createElements(){
+        let main = document.getElementById('ajax');
+        let table = document.createElement('table');
+        table.id = 'ajaxTable' ;
+        main.appendChild(table);
+        this.CreateCols();
+        this.set_lenOfMainArr(this.arr.length);
     }
     /**
-     * @param {array} arr ,{int} bigLoopStart default 0
+     * @param {int} bigLoopStart default 0
      *
      * Create  rows and cols inside ajaxTable
      *
      * @return void
      *
      */
-    CreateRowsCols(arr ,bigLoopStart = 0 ){
+    CreateCols(bigLoopStart = 0){
         let tableId = document.getElementById('ajaxTable');
-        for(let i = bigLoopStart; i < arr.length ;i++){
+        for(let i = bigLoopStart; i < this.arr.length ;i++){
             let row = document.createElement("TR");
             row.className = 'ajaxRow';
             tableId.appendChild(row);
-            for(let j = 0;j < arr[0].length; j++) {
-                if(j % 2 === 0){
+            for(let j = 0;j < this.arr[0].length; j++) {
+                this.createRows(i,j);
+            }
+        }
+    }
 
-                }else {
-                    let col = document.createElement("TD");
-                    let link = document.createElement('a');
-                    link.className ='link';
-                      col.className = 'ajaxCol';
-                    link.innerHTML = this.replace(arr[i][j]);
-                    link.href = 'Posts/'+arr[i][j-1]+'/edit';
-                    document.getElementsByClassName('ajaxRow')[i].appendChild(col);
-                    document.getElementsByClassName('ajaxCol')[i].appendChild(link);
+    /**
+     * @param {int} i , {int} j
+     *
+     * Create Rows
+     *
+     * @return void
+     *
+     */
+    createRows(i,j){
+        if(j % 2 !== 0 && this.searchBool) {
+            let col = document.createElement("TD");
+            col.className = 'ajaxCol';
+            document.getElementsByClassName('ajaxRow')[i].appendChild(col);
+            let link = document.createElement('a');
+            link.className = 'link';
+            link.innerHTML = this.replace(this.arr[i][j]);
+            link.href = 'Posts/' + this.arr[i][j - 1] + '/edit';
+            document.getElementsByClassName('ajaxCol')[i].appendChild(link);
 
+        }
+    }
+
+    /**
+     * Modify the content
+     *
+     * @return void
+     *
+     */
+    modifyElements(){
+        let col = document.getElementsByClassName('link');
+        let count = 0 ;
+        for(let i = 0; i < this.arr.length;i++){
+            for(let j = 0; j < this.arr[0].length;j++)
+            {
+                //escape ids
+                if(this.searchBool) {
+                    if (j % 2 !== 0) {
+                        col[count].innerHTML = this.replace(this.arr[i][j]);
+                        count++;
+                    }
                 }
             }
         }
     }
+
+    /**
+     *
+     * @param  int
+     *
+     * Add or Remove rows
+     *
+     * @return void
+     */
+    addRemove(){
+        let row = document.getElementsByClassName('ajaxRow');
+        //remove and modify
+        if(this.lenOfMainArr > this.arr.length){
+            for(let i = this.lenOfMainArr - 1; i > this.arr.length - 1 ;i--){
+                row[i].parentNode.removeChild(row[i]);
+            }
+        }
+        //add and modify
+        if(this.lenOfMainArr < this.arr.length){
+            this.CreateCols(this.lenOfMainArr);
+        }
+        this.lenOfMainArr = this.arr.length;
+        this.modifyElements();
+
+    }
+
     /**
      * @param {int} lastArrLength
      *
@@ -182,11 +191,8 @@ class Ajax {
      *@return string
      */
     replace(arrText){
-        let name = $('#name').val();
-        let textReplace = arrText.replace(name.trim(),"<strong>"+name.trim()+"</strong>");
+        let textReplace = arrText.replace(this.input.trim(),"<strong>"+this.input.trim()+"</strong>");
         return textReplace ;
     }
-
-
 }
 
