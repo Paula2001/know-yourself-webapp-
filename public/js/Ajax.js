a2db2d8ccf111ba05c66fbdd50df7da3a61c6002
Paula George <polagorge@gmail.com>
2018-12-06 13:47:25 +0200

    ajax interface is done

--- public/js/Ajax.js
+++ public/js/Ajax.js
@@ -5,7 +5,6 @@
  *
  */
 class Ajax {
-
     constructor(searchBool = false ,postBool = false){
         this.arr = [];
         this.lenOfMainArr = null ;
@@ -57,142 +56,8 @@ class Ajax {
      *
      * @return void
      */
-    execute(jsonObj){
-        this.arr =  JSON.parse(jsonObj);
-        if(this.lenOfMainArr === this.arr.length){
-            this.modifyElements();
-        }else if(this.lenOfMainArr === null) {
-            this.createElements();
-        }else if (this.lenOfMainArr !== this.arr.length){
-            this.addRemove();
-        }
-    }
-    /**
-     *Creating Main Elements
-     *
-     * @return void
-     *
-     */
-    createElements(){
-        let main = document.getElementById('ajax');
-        let table = document.createElement('table');
-        table.id = 'ajaxTable' ;
-        main.appendChild(table);
-        this.CreateCols();
-        this.set_lenOfMainArr(this.arr.length);
-    }
-    /**
-     * @param {int} bigLoopStart default 0
-     *
-     * Create  rows and cols inside ajaxTable
-     *
-     * @return void
-     *
-     */
-    CreateCols(bigLoopStart = 0){
-        let tableId = document.getElementById('ajaxTable');
-        for(let i = bigLoopStart; i < this.arr.length ;i++){
-            let row = document.createElement("TR");
-            row.className = 'ajaxRow';
-            tableId.appendChild(row);
-            for(let j = 0;j < this.arr[0].length; j++) {
-                this.createRows(i,j);
-            }
-        }
-    }
-
-    /**
-     * @param {int} i , {int} j
-     *
-     * Create Rows
-     *
-     * @return void
-     *
-     */
-    createRows(i,j){
-        if(j % 2 !== 0 && this.searchBool) {
-            let col = document.createElement("TD");
-            col.className = 'ajaxCol';
-            document.getElementsByClassName('ajaxRow')[i].appendChild(col);
-            let link = document.createElement('a');
-            link.className = 'link';
-            link.innerHTML = this.replace(this.arr[i][j]);
-            link.href = 'Posts/' + this.arr[i][j - 1] + '/edit';
-            document.getElementsByClassName('ajaxCol')[i].appendChild(link);
-
-        }
-    }
-
-    /**
-     * Modify the content
-     *
-     * @return void
-     *
-     */
-    modifyElements(){
-        let col = document.getElementsByClassName('link');
-        let count = 0 ;
-        for(let i = 0; i < this.arr.length;i++){
-            for(let j = 0; j < this.arr[0].length;j++)
-            {
-                //escape ids
-                if(this.searchBool) {
-                    if (j % 2 !== 0) {
-                        col[count].innerHTML = this.replace(this.arr[i][j]);
-                        count++;
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     *
-     * @param  int
-     *
-     * Add or Remove rows
-     *
-     * @return void
-     */
-    addRemove(){
-        let row = document.getElementsByClassName('ajaxRow');
-        //remove and modify
-        if(this.lenOfMainArr > this.arr.length){
-            for(let i = this.lenOfMainArr - 1; i > this.arr.length - 1 ;i--){
-                row[i].parentNode.removeChild(row[i]);
-            }
-        }
-        //add and modify
-        if(this.lenOfMainArr < this.arr.length){
-            this.CreateCols(this.lenOfMainArr);
-        }
-        this.lenOfMainArr = this.arr.length;
-        this.modifyElements();
-
-    }
-
-    /**
-     * @param {int} lastArrLength
-     *
-     * setter for lenOfMainArr
-     *
-     * @return void
-     *
-     */
-    set_lenOfMainArr(lastArrLength){
-        this.lenOfMainArr = lastArrLength ;
-    }
-
-    /**
-     * @param {string} arrText
-     *
-     * replace function
-     *
-     *@return string
-     */
-    replace(arrText){
-        let textReplace = arrText.replace(this.input.trim(),"<strong>"+this.input.trim()+"</strong>");
-        return textReplace ;
+    execute(){
+        console.log(this.arr);
     }
 }
-
+module.exports = Ajax ;
