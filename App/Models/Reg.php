e2d734e428292f807600a767465ce45a71181321
Paula George <polagorge@gmail.com>
2018-12-11 16:33:12 +0200

    registration model is finished 2 queries

--- App/Models/Reg.php
+++ App/Models/Reg.php
@@ -7,11 +7,19 @@
             $query = "INSERT INTO
              `users`(`id`, `first_name`, `last_name`, `password`, `email`, `age`, `gender`, `time_account_created`)
                 VALUES (?,?,?,?,?,?,?,?)";
-                $df = new mysqli();
             $stmt = $dp->prepare($query);
-            $stmt->bind_param('isssssis',$id,$firstName,$lastName,$passwd,$email,$age,$gender,$currentTime);
+            $stmt->bind_param('ssssssis',$id,$firstName,$lastName,$passwd,$email,$age,$gender,$currentTime);
             $stmt->execute();
-            echo  $stmt->affected_rows ; 
+            return  ($stmt->affected_rows) ? true : false ;
         }
-
+        public static function checkEmail(string $emailInput){
+            $dp = self::getDB();
+            $emailInput = $emailInput . '%';
+            $query = "SELECT email FROM users WHERE email LIKE  ?  ";
+            $stmt = $dp->prepare($query);
+            $stmt->bind_param('s',$emailInput);
+            $stmt->execute();
+            $stmt->bind_result($emailResult);
+            return ($stmt->fetch()) ? $emailResult : $emailResult  ;
+        }
     }
