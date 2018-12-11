0a5cdd75140b33def03542a25bec99403bb7098b
Paula George <polagorge@gmail.com>
2018-12-11 16:24:36 +0200

    most work is finished but haven't escaped any fields

--- App/Controllers/Registration.php
+++ App/Controllers/Registration.php
@@ -11,6 +11,10 @@ class Registration extends \Core\Controller{
     */
     public function indexAction(){
 
+        if(isset($_POST['ajax'])) {
+            $resultEncode = json_encode(Reg::checkEmail($_POST['name']));
+            $_SESSION['data'] = $resultEncode;
+        }
         View::render('Registration/index.php');
     }
     /**
@@ -22,11 +26,12 @@ class Registration extends \Core\Controller{
     */
     public function completeAction(){
         if($this->modelProcessing()){
-            $id = date('siHmd') - rand(111111111,99999999);
+            $id = $this->generateRandom();
             $age = $_POST['years'] .'-'. $_POST['months'] .'-'. $_POST['days'];
             $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
             $currentTime = date('Y/m/d-H:i:s');
-            Reg::insertData($id,
+
+            if(Reg::insertData($id,
             $_POST['firstName'],
             $_POST['lastName'],
             $password,
@@ -34,9 +39,12 @@ class Registration extends \Core\Controller{
             $age ,
             $_POST['gender'],
             $currentTime
-            );
-        }
+            )){
+                echo '<p class="success">You\'ve joined our big famous yarab sam7ne 3ala 2l kdb dh website</p>' ;
+                echo '<a href="/home/index" class="success">click here to login</a>' ;
+            };
 
+        }
         View::render('Registration/complete.php');
     }
     /**
@@ -53,7 +61,29 @@ class Registration extends \Core\Controller{
                 return false ;
             }
         }
-
         return true ;
+    }
+
+    /**
+     * generate random id consist of email as a salt factor
+     *
+     * @return string
+     */
+    private function generateRandom(){
+
+        $string = $_POST['email'];
+        $string = str_replace("@","",$string);
+        $string = preg_replace("/\.[a-zA-Z]+/","",$string);
+        $stringLen = strlen($string);
+        $result = null ;
+        $arr  = array('s' ,'i','H', 'm' ,'d');
+        for($i = 0;$i < 3;$i++){
+            $randomStr = null ;
+            for ($j = 0;$j < 2;$j++){
+                $randomStr .= $string[rand(0,$stringLen - 1)] ;
+            }
+            $result .= date($arr[$i]) . $randomStr ;
+        }
+        return $result ;
     }
 }
