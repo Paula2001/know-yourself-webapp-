0efff2b6c196f4a3b37779568bfeae2ff74064f8
Paula George <polagorge@gmail.com>
2018-12-11 18:11:16 +0200

    id regex is edited

--- public/index.php
+++ public/index.php
@@ -37,7 +37,7 @@ $router = new Core\Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
-$router->add('{controller}/{id:\d+}/{action}');
+$router->add('{controller}/{id:\w+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 $router->dispatch($_SERVER['QUERY_STRING']);
 
