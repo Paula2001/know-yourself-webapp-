c090416cc24c7223f7a911bf7b328f90e5c45b62
Paula George <polagorge@gmail.com>
2018-12-12 22:50:09 +0200

    reg index

--- App/Views/Registration/index.php
+++ App/Views/Registration/index.php
@@ -8,7 +8,7 @@ echo BaseView::header('Registration',['registration','main']);
 <body>
 
 <h1 id="title">Create a new account</h1>
-<form id="regForm" name="reg" method="post" action="complete">
+<form id="regForm" action="complete" method="POST" enctype="multipart/form-data" >
     <div id="errorContainer"></div>
     <div id="textArea">
 
@@ -20,15 +20,15 @@ echo BaseView::header('Registration',['registration','main']);
     <br>
     <br>
     <gender>
-        <input class="gender"   type="radio" value="1" name="gender">
+        <input class="gender"  type="radio" value="1" name="gender">
         <label>Male</label>
         <input class="gender" checked type="radio" value="0" name="gender">
         <label>Female</label>
     </gender>
-    <br>
-    <br>
+    <h2>Upload your profile picture</h2>
+    <input id="file_upload" type="file" name="image">
     <p id="btnText">**This button is disabled till you finish the form</p>
-    <input value="Sign Up"  disabled  id="submit" type="submit">
+    <input value="Sign Up"  disabled  id="submit" type="submit" name="submit">
 
 </form>
 <script src=/js/registration/registrationBin.js type='text/javascript'></script>
