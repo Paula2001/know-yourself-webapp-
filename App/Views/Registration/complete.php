61e03d885d0e7d950817fafad59bb9712d765444
Paula George <polagorge@gmail.com>
2018-12-12 22:39:49 +0200

    complete view is finished

---
+++ App/Views/Registration/complete.php
@@ -0,0 +1,16 @@
+<?php
+use App\Views\BaseView;
+$file = dirname(__DIR__) . '/'.'BaseView.php';
+require_once $file ;
+echo BaseView::header('Complete Registration',['registration','main' ,'profileStyling']);
+
+?>
+<h2 id="mainText">We are so happy that you joined us ;) ,This your profile data</h2>
+<div id="profile_container">
+    <img id="profile_img" src="<?php echo '/uploads/'.$args[0] ;  ?>">
+    <h2 class="profile_text">Name : <?php echo $args[1]; ?> </h2>
+    <h2 class="profile_text">Email : <?php echo $args[2]; ?> </h2>
+    <h2 class="profile_text">Date of birth : <?php echo $args[3]; ?> </h2>
+    <h2 class="profile_text">Gender : <?php echo $args[4]; ?> </h2>
+    <a id="proceed" href="#">Proceed to login</a>
+</div>
