da5f05acc3eedbc7bfa1057aa87d835f9b842b08
Paula George <polagorge@gmail.com>
2018-12-17 22:02:23 +0200

    minor

--- App/Views/Registration/complete.php
+++ App/Views/Registration/complete.php
@@ -12,5 +12,5 @@ echo BaseView::header('Complete Registration',['registration','main' ,'profileSt
     <h2 class="profile_text">Email : <?php echo $args[2]; ?> </h2>
     <h2 class="profile_text">Date of birth : <?php echo $args[3]; ?> </h2>
     <h2 class="profile_text">Gender : <?php echo $args[4]; ?> </h2>
-    <a id="proceed" href="#">Proceed to login</a>
+    <a id="proceed" href="/login/index">Proceed to login</a>
 </div>
